713d035a485f5e8ccc825f7b9c9fefac730096c9
ThomazBartol <thomaz.bartol2006@gmail.com>
2025-05-21 07:48:05 -0300

    feat: create entities Moto and Yard

---
+++ SentinelTrack/Domain/Entities/Moto.cs
@@ -0,0 +1,17 @@
+﻿namespace SentinelTrack.Domain.Entities
+{
+    public class Moto
+    {
+        public Guid Id { get; private set; }
+
+        public string Plate { get; private set; }
+
+        public string? Model { get; private set; }
+
+        public string? Color { get; private set; }
+
+        public int YardId { get; set; }       
+       
+        public Yard Yard { get; set; }
+    }
+}
---
+++ SentinelTrack/Domain/Entities/Yard.cs
@@ -0,0 +1,17 @@
+﻿namespace SentinelTrack.Domain.Entities
+{
+    public class Yard
+    {
+        public Guid Id { get; private set; }
+
+        public string Name { get; private set; }
+
+        public string? Address { get; set; }  
+      
+        public string? PhoneNumber { get; set; } 
+      
+        public int? Capacity { get; set; }
+
+        public List<Moto> Motos { get; set; } = new List<Moto>();
+    }
+}
