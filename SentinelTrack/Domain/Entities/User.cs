a455b72199d933f83d3815ffb555f68e0b57d668
ThomazBartol <thomaz.bartol2006@gmail.com>
2025-09-30 03:13:07 -0300

    feat: create User entity

---
+++ SentinelTrack/Domain/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿namespace SentinelTrack.Domain.Entities
+{ 
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = default!;
+        public string Email { get; set; } = default!;
+        public string Role { get; set; } = "user";
+    }
+}
